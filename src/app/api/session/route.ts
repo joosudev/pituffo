e48a4cf991aa64ebfcb98f82c1b56293d5c979e0
joosudev <vwillians22@gmail.com>
2025-09-03 23:20:41 -0500

    Configuracion de env

--- src/app/api/session/route.ts
+++ src/app/api/session/route.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { NextRequest, NextResponse } from "next/server";
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
+ 
 export async function GET(request: NextRequest) {
   try {
     // Aquí normalmente se validaría la sesión
